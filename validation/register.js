1cdf531b1ff326ae5b77d374b6cf746187124afe
sylvanou <sylvanoumbac24@gmail.com>
2018-05-16 12:42:46 -0400

    added input validation to Register component

--- validation/register.js
+++ validation/register.js
@@ -20,25 +20,25 @@ module.exports = function validateRegisterInput(data) {
     errors.name = 'Name field is required';
   }
 
-  // Check if email is empty
-  if(Validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
-  }
-
   // Check if input is an email
   if(!Validator.isEmail(data.email)) {
     errors.email = 'Email is invalid';
   }
 
-  // Check if password is empty
-  if(Validator.isEmpty(data.password)) {
-    errors.password = 'Password field is required';
+  // Check if email is empty
+  if(Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
   }
-  
+
   // Check if password is at least 6, but not greater than 30
   if(!Validator.isLength(data.password, {min: 6, max: 30})) {
     errors.password = 'Password must be at least 6 characters';
   }
+
+  // Check if password is empty
+  if(Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }  
 
   // Check if confirmation password is empty
   if(Validator.isEmpty(data.password2)) {
